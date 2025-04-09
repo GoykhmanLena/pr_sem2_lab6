ac8498797d85e49f511fbd83ebf86c90a3b45cf5
GoykhmanLena <lenagojhman@gmail.com>
2025-04-09 09:40:11 +0400

    big files support

--- lab-common/src/main/java/ru/lenok/common/util/SerializationUtils.java
+++ lab-common/src/main/java/ru/lenok/common/util/SerializationUtils.java
@@ -1,15 +1,21 @@
 package ru.lenok.common.util;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SerializationUtils {
-    public static final int BUFFER_SIZE = 65535;
+    public static final int BUFFER_SIZE = 65000;
+    public static final int MAX_CHUNK_SIZE = 40000;
     public static final SerializationUtils INSTANCE = new SerializationUtils();
-    private SerializationUtils(){}
-    public Object deserialize(byte[] data){
+
+    private SerializationUtils() {
+    }
+
+    public Object deserialize(byte[] data) {
         try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data))) {
             Object obj = ois.readObject();
-            // System.out.println("Объект десериализован: " + obj);
             return obj;
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
@@ -17,7 +23,7 @@ public class SerializationUtils {
         }
     }
 
-    public byte[] serialize(Object obj){
+    public byte[] serialize(Object obj) {
         try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(BUFFER_SIZE);
              ObjectOutputStream oos = new ObjectOutputStream(byteArrayOutputStream)) {
             oos.writeObject(obj);
@@ -28,4 +34,43 @@ public class SerializationUtils {
             return null;
         }
     }
+
+    public List<byte[]> serializeAndSplitToChunks(Object obj) {
+        byte[] bytes = serialize(obj);
+        return splitToChunks(bytes);
+    }
+
+    private List<byte[]> splitToChunks(byte[] data) {
+        List<byte[]> chunks = new ArrayList<>();
+        int start = 0;
+        while (start < data.length) {
+            int end = Math.min(data.length, start + MAX_CHUNK_SIZE);
+            byte[] chunk = Arrays.copyOfRange(data, start, end);
+            chunks.add(chunk);
+            start = end;
+        }
+        return chunks;
+    }
+
+    public Object deserializeFromChunks(List<byte[]> chunks) {
+        byte[] bytes = mergeChunks(chunks);
+        return deserialize(bytes);
+    }
+
+    private byte[] mergeChunks(List<byte[]> chunks) {
+        int totalLength = 0;
+        for (byte[] chunk : chunks) {
+            totalLength += chunk.length;
+        }
+
+        byte[] result = new byte[totalLength];
+        int offset = 0;
+        for (byte[] chunk : chunks) {
+            System.arraycopy(chunk, 0, result, offset, chunk.length);
+            offset += chunk.length;
+        }
+
+        return result;
+    }
+
 }
--- server-sender/src/main/java/ru/lenok/server_sender/ServerResponseSender.java
+++ server-sender/src/main/java/ru/lenok/server_sender/ServerResponseSender.java
@@ -8,24 +8,38 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.List;
 
 import ru.lenok.common.util.SerializationUtils;
 
 @Data
 public class ServerResponseSender {
     private static final Logger logger = LoggerFactory.getLogger(ServerResponseSender.class);
+    private final SerializationUtils serializer = SerializationUtils.INSTANCE;
     private final DatagramSocket socket;
 
     public ServerResponseSender(DatagramSocket datagramSocket) {
         this.socket = datagramSocket;
     }
 
-    public void sendMessageToClient(Object response, InetAddress clientIp, int clientPort) throws IOException {
-        byte[] responseData = SerializationUtils.INSTANCE.serialize(response);
-        DatagramPacket responsePacket = new DatagramPacket(
-                responseData, responseData.length, clientIp, clientPort);
+    public void sendMessageToClient(Object response, InetAddress clientIp, int clientPort) throws IOException, InterruptedException {
+        List<byte[]> chunks = serializer.serializeAndSplitToChunks(response);
+        int chunkSize = chunks.size();
+        byte[] chunksSizeSerialized = serializer.serialize(new Integer(chunkSize));
 
+        DatagramPacket responsePacket = new DatagramPacket(chunksSizeSerialized, chunksSizeSerialized.length, clientIp, clientPort);
         socket.send(responsePacket);
-        logger.debug("Отправлены данные: " + response);
+        logger.debug("Отправлено количество чанков: " + chunkSize);
+        int i = 0;
+        for (byte[] responseDataChunk : chunks) {
+            responsePacket = new DatagramPacket(responseDataChunk, responseDataChunk.length, clientIp, clientPort);
+            socket.send(responsePacket);
+            i++;
+            Thread.sleep(50);
+            logger.debug("Отправлен чанк " + i + " из " + chunkSize);
+        }
+        if (chunkSize == 1) {
+            logger.debug("Отправлены данные: " + response);
+        }
     }
 }
